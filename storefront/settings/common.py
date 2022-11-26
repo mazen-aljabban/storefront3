66cc4231c10d566d6be50d5082e8ec857eba49f2
tiger <tiger@tiger.tiger>
2022-11-26 14:10:07 +0300

    initial commit

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -176,7 +176,6 @@ ADMINS = [
     ('Tiger', 'admin@tiger.com')
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
@@ -185,15 +184,7 @@ CELERY_BEAT_SCHEDULE = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+
 
 LOGGING = {
     'version': 1,
--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -10,8 +10,30 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
-        'HOST': 'localhost',
+        'HOST': 'mysql',
         'USER': 'root',
         'PASSWORD': 'tiger1'
     }
+}
+
+CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+EMAIL_HOST = 'smtp4dev'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request:True
 }
